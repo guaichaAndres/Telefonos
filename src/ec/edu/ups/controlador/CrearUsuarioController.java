039889ccb31d67d7db62ac991121f778b0491155
guaichaAndres <56601601+guaichaAndres@users.noreply.github.com>
2021-05-14 04:38:24 -0500

    Creacion de usuarios

--- src/ec/edu/ups/controlador/CrearUsuarioController.java
+++ src/ec/edu/ups/controlador/CrearUsuarioController.java
@@ -11,7 +11,7 @@ import ec.edu.ups.dao.DAOFactory;
 import ec.edu.ups.dao.UsuarioDAO;
 import ec.edu.ups.modelo.Usuario;
 
-@WebServlet("/CrearPersonaController")
+@WebServlet("/CrearUsuarioController")
 public class CrearUsuarioController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private UsuarioDAO usuarioDao;
